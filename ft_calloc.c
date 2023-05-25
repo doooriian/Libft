9f0480ec36422ad7ab77266a196a91f8a100572b
doooriian <dorian.ley91@gmail.com>
2023-05-25 13:10:48 +0200

    changement header 42

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,32 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dley <dley@student.42.fr >                 +#+  +:+       +#+        */
+/*   By: dley <dley@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 22:00:39 by dley              #+#    #+#             */
-/*   Updated: 2023/05/11 16:33:00 by dley             ###   ########.fr       */
+/*   Updated: 2023/05/25 13:03:28 by dley             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/*
-void	*ft_calloc(size_t nmemb, size_t size)
-{
-	void	*str;
-
-	if (size == 0 || size == 0)
-	{
-		nmemb = 1;
-		size = 1;
-	}
-	str = malloc(nmemb * size);
-	if (str == NULL)
-		return (NULL);
-	ft_bzero(str, nmemb);
-	return (str);
-}
-*/
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
