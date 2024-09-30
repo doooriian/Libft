19f209ccb3dc35f426fbda05e8d9a2e987111f9a
Doooriian <dorian.ley91@gmail.com>
2024-09-30 22:31:43 +0200

    first push

---
+++ ft_calloc.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doley <doley@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 22:01:04 by doley             #+#    #+#             */
+/*   Updated: 2024/09/30 22:24:51 by doley            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	size_t	total;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	if (nmemb > SIZE_MAX / size)
+		return (NULL);
+	total = nmemb * size;
+	ptr = malloc(total);
+	if (!ptr)
+		return (NULL);
+	ft_memset(ptr, 0, total);
+	return (ptr);
+}
