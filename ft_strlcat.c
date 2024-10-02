632e3d58aba395d5b2c84b4c7b26de553cba5690
Doooriian <dorian.ley91@gmail.com>
2024-10-02 20:24:38 +0200

    add atoi to strlen

---
+++ ft_strlcat.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doley <doley@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 19:41:58 by doley             #+#    #+#             */
+/*   Updated: 2024/10/02 20:13:19 by doley            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	dest_len;
+	size_t	src_len;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (size <= dest_len)
+		return (dest_len + src_len);
+	i = dest_len;
+	j = 0;
+	while (src[j] && i < size - 1)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest_len + src_len);
+}
