8f64ec97dc6c83767968c37abeb26ac10e54bd08
Doooriian <dorian.ley91@gmail.com>
2024-10-09 01:24:17 +0200

    add first part plus proto in header

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: doley <doley@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 21:29:39 by doley             #+#    #+#             */
-/*   Updated: 2024/09/30 22:21:20 by doley            ###   ########.fr       */
+/*   Updated: 2024/10/09 01:23:58 by doley            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,30 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdint.h>
+
+/* FUNCTIONS */
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+char	*ft_strdup(const char *s1);
+char	*ft_strchr(const char *s, int c);
+void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_isprint(int c);
+int		ft_isdigit(int c);
+int		ft_isascii(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+void	*ft_calloc(size_t nmemb, size_t size);
+void	ft_bzero(void *s, size_t n);
+int		ft_atoi(char *str);
 
 #endif
