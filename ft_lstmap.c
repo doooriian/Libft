ee882a9a1fecc2efe86ac4eef36475f65fdf6faa
Doooriian <dorian.ley91@gmail.com>
2024-10-15 17:49:17 +0200

    correction add in lstmap, del the content

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: doley <doley@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 19:41:55 by doley             #+#    #+#             */
-/*   Updated: 2024/10/11 21:37:25 by doley            ###   ########.fr       */
+/*   Updated: 2024/10/15 17:48:35 by doley            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
 	t_list	*new_element;
+	void	*new_content;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_lst = NULL;
 	while (lst)
 	{
-		new_element = ft_lstnew(f(lst->content));
+		new_content = f(lst->content);
+		new_element = ft_lstnew(new_content);
 		if (!new_element)
 		{
+			del(new_content);
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
